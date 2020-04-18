98c0fb1ae2f47167eaaee7bb9119773101b2a09e
Sagar Jon <sagar@ficode.in>
2020-04-18 17:32:01 +0530

    third commit

--- src/pages/LogIn.js
+++ src/pages/LogIn.js
@@ -10,7 +10,7 @@ export default class LogIn extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      userName:'',
+      userName:'',           
       password:'',
       nameValidate:true,
       passwordValidate:true,
